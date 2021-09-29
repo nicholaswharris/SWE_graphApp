67060b379c060d4f5199cafd54d22628395cbf92
garrett-london <glondon@unca.edu>
2021-09-29 13:25:41 -0400

    Update App.java
    Added a sound effect. (Sorry.) Really Hoping this doesnt mess anything up or cause a merge conflict, etc. Probably will. We'll see.

--- src/edu/unca/csci338/App.java
+++ src/edu/unca/csci338/App.java
@@ -1,18 +1,57 @@
 package edu.unca.csci338;
 
+import java.applet.*;
 import java.awt.Color;
 import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
+import java.lang.System.Logger;
+import java.net.MalformedURLException;
+import java.net.URL;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.*;
 
 public class App {
-	public static void main(String[] args) {
+	File wavFile;
+    static URL defaultSound;
+    public static Clip clip;
+    public static AudioInputStream audioInputStream;
+	public static void play() {
+		try {
+            audioInputStream = AudioSystem.getAudioInputStream(defaultSound);
 
+            try {
+                clip = AudioSystem.getClip();
+                clip.open(audioInputStream);
+                clip.loop(20000);
+                clip.start();
+                try{
+                	Thread.sleep(1000);
+                }catch(InterruptedException l) {
+                	System.out.println("You done it wrong");
+                }
+                clip.stop();
+            } catch (LineUnavailableException e) {
+            }
+
+        } catch (UnsupportedAudioFileException | IOException e) {
+        }
+	}
+
+	public static void main(String[] args) {
 		JFrame f = new JFrame("CSCI338 Graphical Application");
 
 		f.getContentPane().setBackground(Color.YELLOW);
 		JLabel label1 = new JLabel("I'll never change.");
 		label1.setBounds(150, 200, 200, 20);
-
+		try {defaultSound = new URL ("https://www.soundjay.com/human/fart-01.wav");}
+		catch (MalformedURLException ex) {
+            System.out.println("Fart no workie.");
+        }
 		JButton red = new JButton("Make Red");
 		JButton green = new JButton("Make Green");
 		JButton text = new JButton("Change Text");
@@ -21,6 +60,7 @@ public class App {
 		red.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				f.getContentPane().setBackground(Color.RED);
+				play();
 			}
 		});
 
@@ -28,6 +68,7 @@ public class App {
 		green.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				f.getContentPane().setBackground(Color.GREEN);
+				play();
 			}
 		});
 
